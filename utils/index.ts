b239a30ea77084a30bed0c9dc44c0e18e469dbe7
appdrop-team <support@appdrop.com>
2021-03-03 10:28:43 -0600

    patches util functions

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,4 @@
-import { CreateEventParams } from '../analytics';
-import { CreateUserParams, UpdateUserParams } from '../auth';
 import * as Projects from '../projects';
-
 export const NUM_FREE_INVOCATIONS = 50000;
 export interface ERROR_STATUS_CODE_KEYS {
     BAD_REQUEST: any;
@@ -27,7 +24,6 @@ export const ERROR_STATUS_CODES: {
     INTERNAL_SERVER_ERROR: 500,
     SERVICE_UNAVAILABLE: 503,
 });
-
 export interface ERROR_RESPONSE_KEYS {
     APP_CONFIG_ERROR: any;
     APP_ID_ERROR: any;
@@ -42,71 +38,71 @@ export interface ERROR_RESPONSE_KEYS {
     USER_ID_INVALID: any;
 }
 export const ERROR_RESPONSES: {
-    [key in keyof ERROR_RESPONSE_KEYS]: Projects.Base.API_Request_Error;
+    [key in keyof ERROR_RESPONSE_KEYS]: Projects.Base.APIRequestError;
 } = Object.freeze({
     APP_CONFIG_ERROR: {
         created: null,
-        error_code: 'app-config-error',
+        error_type: 'app-config-error',
         message: 'The request did not include a valid app config object',
         status_code: ERROR_STATUS_CODES.UNAUTHORIZED,
     },
     APP_ID_ERROR: {
         created: null,
-        error_code: 'app-id-error',
+        error_type: 'app-id-error',
         message: 'The app id included in this request is not associated with your API key',
         status_code: ERROR_STATUS_CODES.UNAUTHORIZED,
     },
     API_KEY_EXPIRED: {
         created: null,
-        error_code: 'api-key-expired',
+        error_type: 'api-key-expired',
         message: 'The included API key is expired',
         status_code: ERROR_STATUS_CODES.FORBIDDEN,
     },
     API_KEY_INVALID: {
         created: null,
-        error_code: 'api-key-invalid',
+        error_type: 'api-key-invalid',
         message: 'The included API key is not associated with an active Appdrop account',
         status_code: ERROR_STATUS_CODES.FORBIDDEN,
     },
     API_KEY_MISSING: {
         created: null,
-        error_code: 'api-key-missing',
+        error_type: 'api-key-missing',
         message: 'The request did not include an API key',
         status_code: ERROR_STATUS_CODES.UNAUTHORIZED,
     },
     API_KEY_REVOKED: {
         created: null,
-        error_code: 'api-key-revoked',
+        error_type: 'api-key-revoked',
         message: 'The included API key has been revoked',
         status_code: ERROR_STATUS_CODES.FORBIDDEN,
     },
     INTERNAL_SERVER_ERROR: {
         created: null,
-        error_code: 'internal-server-error',
+        error_type: 'internal-server-error',
         message: 'Our server encountered a run-time error when processing your request',
         status_code: ERROR_STATUS_CODES.INTERNAL_SERVER_ERROR,
     },
     INVALID_DATA_PROPERTY: {
         created: null,
-        error_code: 'invalid-data-property',
+        error_type: 'invalid-data-property',
         message: 'Your data property was undefined or formatted incorrectly. Please refer to our docs to correct this issue.',
         status_code: ERROR_STATUS_CODES.BAD_REQUEST,
     },
     RATE_LIMIT_SURPASSED: {
         created: null,
-        error_code: 'rate-limit-surpassed',
+        error_type: 'rate-limit-surpassed',
         message: 'You have reached the daily rate limit for the Starter Plan. Activate billing at https://appdrop.com/pricing',
         status_code: ERROR_STATUS_CODES.TOO_MANY_REQUESTS,
     },
     UNKNOWN_ERROR: {
         created: null,
-        error_code: 'unknown-error',
+        error_type: 'unknown-error',
         message: 'Our server encountered an unknown error when processing your request',
         status_code: ERROR_STATUS_CODES.BAD_REQUEST,
     },
     USER_ID_INVALID: {
         created: null,
-        error_code: 'user-id-invalid',
+        error_type: 'user-id-invalid',
         message: 'There is no user in your project with this id.',
         status_code: ERROR_STATUS_CODES.BAD_REQUEST,
     },
@@ -121,72 +117,6 @@ export function RandString(l = -1) {
         );
     }
     return result;
-}
-// export function ValidConfig(app_config: Appdrop.Projects.AppConfig) {
-export function ValidConfig(app_config: Projects.Base.AppConfig) {
-    return (
-        app_config !== undefined &&
-        app_config.api_key !== undefined &&
-        typeof app_config.api_key === 'string' &&
-        app_config.app_id !== undefined &&
-        typeof app_config.app_id === 'string' &&
-        app_config.project_id !== undefined &&
-        typeof app_config.project_id === 'string'
-    );
-}
-export function ValidParams(
-    params: 
-    CreateEventParams|
-    CreateUserParams|
-    UpdateUserParams |
-    undefined |
-    null,
-    action:
-    'Create' |
-    'Update',
-    object:
-    'Event' |
-    'User'
-) {
-    // Fix this. 
-    // Current implementation is to spread all properties passed.
-    // There exist bug vectors where a develoepr could
-    // pass incorrect data types -> 
-    // corrupt data -> and 
-    // crash their Appdrop dashboard
-    // const invalid_keys = Object.keys(update_user_params).filter(k => ['Fix this.'].includes(k));
-    // for (const k of invalid_keys) {
-    //     delete (update_user_params as any)[k];
-    // }
-    if (params !== undefined && params !== null) {
-        if (action === 'Create') {
-            if (object === 'Event') {
-                const create_event_params = params as CreateEventParams;
-                const keys = Object.keys(create_event_params);
-                return (
-                    keys.includes('name') && 
-                    typeof create_event_params['name'] === 'string'
-                );
-            }
-            else if (object === 'User') {
-                const create_user_params = params as CreateUserParams;
-                const keys = Object.keys(create_user_params);
-                return (
-                    keys.includes('id') &&
-                    typeof create_user_params['id'] === 'string'
-                );
-            }
-        }
-        else if (action === 'Update') {
-            if (object === 'User') {
-                return (
-                    (params as any)['id'] === undefined &&
-                    (params as any)['livemode'] === undefined
-                );
-            }
-        }
-    }
-    return false;
 }
 export const month_map = (n: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11, long: boolean) => {
 	switch (n) {
@@ -218,7 +148,6 @@ export const month_map = (n: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11, lo
 			throw new Error('invalid month number passed to month_map');
 	}
 };
-
 export const validString = (s: string|null|undefined, l: boolean) => {
     return (
         s !== null &&
